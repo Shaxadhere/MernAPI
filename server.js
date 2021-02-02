04d3577fe319efc18f65cca4587783e9f71fe126
Shaxadhere <Shaxad.here@gmail.com>
2021-02-02 11:10:53 +0500

    inital commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+
+
+//Connectivity
+const connectionString = "mongodb+srv://admin:dGakD2MeWQprWN02@cluster0.stgq9.mongodb.net/tinderdb?retryWrites=true&w=majority";
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        console.log("Connected To Database")
+        const db = client.db('tinderdb')
+        const userCollection = db.collection('users')
+
+    //Middlewares
+    app.use(bodyParser.urlencoded({extended:true}))
+    app.use(bodyParser.json())
+    app.use(express.json());
+    
+
+    //Routes
+    app.get('/', (req, res) => {
+        res.status(200).send("Hello World!!!!!!!!!!!!!!!!!!")
+    })
+
+    app.get('/tinder/users', (req, res) => {
+        db.collection('users')
+        .find()
+        .toArray()
+        .then(result =>{
+            res.status(200).send(result)
+        })
+    })
+
+    app.post('/tinder/users', (req, res) => {
+        userCollection.insertOne(req.body)
+        .then(user => {
+            res.status(200).send(user)
+        })
+    })
+
+    app.delete('/tinder/users', (req, res) => {
+        userCollection.findOneAndDelete({
+            name:req.body.name
+        }).then(result => {
+            res.status(200).send(result);
+        }).catch(error => console.error(error))
+    })
+
+    //Listen
+    app.listen(8001, () => { console.log("Listening on port " + 8001) })
+})
+.catch(console.error)
